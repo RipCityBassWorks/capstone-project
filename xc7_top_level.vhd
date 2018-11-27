4b13ce4f841003b45c29f0be1c9c0725a165e31a
Stefan Andersson <ripcitybassworks@gmail.com>
2018-11-26 23:32:52 -0700

    Changed components to reflect board switch

--- xc7_top_level.vhd
+++ xc7_top_level.vhd
@@ -4,8 +4,8 @@
 -- Create Date: 10/18/2018 04:38:16 PM
 -- Design Name: xc7_top_level.vhd
 -- Module Name: xc7_top_level - xc7_top_level_arch
--- Project Name: capstone-fpga-memory-model 
--- Target Devices: XC7A35TICSG324-1L
+-- Project Name: capstone-project
+-- Target Devices: XC7A35TCPG236-1
 -- Tool Versions: Vivado 2018.2
 -- Description: Top level file
 ----------------------------------------------------------------------------------
@@ -18,39 +18,21 @@ use IEEE.NUMERIC_STD.all;
 entity xc7_top_level is
     Port(--Clock signal
         CLK100MHZ           : in    std_logic;
-        --Reset signal
-        ck_rst              : in    std_logic;
         --Switches
-        sw                  : in    std_logic_vector(3 downto 0);
+        sw                  : in    std_logic_vector(15 downto 0);
         --LEDs
-        led                 : out   std_logic_vector(3 downto 0);
-        --RGB LEDs
-        led0_b              : out	std_logic;
-        led0_g              : out	std_logic;
-        led0_r              : out	std_logic;
-        led1_b              : out	std_logic;
-        led1_g              : out	std_logic;
-        led1_r              : out	std_logic;
-        led2_b              : out	std_logic;
-        led2_g              : out	std_logic;
-        led2_r              : out	std_logic;
-        led3_b              : out	std_logic;
-        led3_g              : out	std_logic;
-        led3_r              : out	std_logic;
+        led                 : out   std_logic_vector(15 downto 0);
+        --Seven Segment Display
+        seg                 : out   std_logic_vector(6 downto 0);
+        dp                  : out   std_logic;
+        an                  : out   std_logic_vector(3 downto 0);
         --Buttons
-        btn                 : in    std_logic_vector(3 downto 0);
-        --Pmod Header JA
-        ja                  : out   std_logic_vector(7 downto 0);
-        --Pmod Header JB
-        jb                  : out   std_logic_vector(7 downto 0);
-        --Pmod Header JC
-        jc                  : out   std_logic_vector(7 downto 0);
-        --Pmod Header JD
-        jd                  : out   std_logic_vector(7 downto 0);
-        UART_TXD            : out   std_logic
+        btn                 : in    std_logic_vector(4 downto 0);
+        
+        --PMOD Ports Removed Temp.
+        
         --USB-UART Interface
-        --uart_rxd_out        : out   std_logic;
-        --uart_txd_in         : in    std_logic
+        UART_TXD            : out   std_logic
     );
 end entity xc7_top_level;
 
@@ -58,34 +40,22 @@ architecture xc7_top_level_arch of xc7_top_level is
     
 --COMPONENT DECLARATIONS
     component clock_divider is
-    port(
-        clk_in              : in    std_logic;
-        reset               : in    std_logic;
-        sel                 : in    std_logic_vector(1 downto 0);
-        clk_out             : out   std_logic
-    );
+        port(
+            clk_in              : in    std_logic;
+            reset               : in    std_logic;
+            sel                 : in    std_logic_vector(1 downto 0);
+            clk_out             : out   std_logic
+        );
     end component clock_divider;
     
-    component led_decoder is
-        port(
+    component char_decoder is
+        Port(
             clk             : in    std_logic;
             reset           : in    std_logic;
-            mem_block       : in    std_logic_vector(7 downto 0);
-            led             : out   std_logic_vector(3 downto 0);               
-            led0_b          : out   std_logic;
-            led0_g          : out   std_logic;
-            led0_r          : out   std_logic;
-            led1_b          : out   std_logic;
-            led1_g          : out   std_logic;
-            led1_r          : out   std_logic;
-            led2_b          : out   std_logic;
-            led2_g          : out   std_logic;
-            led2_r          : out   std_logic;
-            led3_b          : out   std_logic;
-            led3_g          : out   std_logic;
-            led3_r          : out   std_logic
+            bin_in          : in    std_logic_vector(15 downto 0);
+            hex_out         : out   std_logic_vector(6 downto 0) 
         );
-    end component led_decoder;
+    end component char_decoder;
     
     component lfsr is
         port(
@@ -118,17 +88,20 @@ architecture xc7_top_level_arch of xc7_top_level is
     end component btn_debounce;
     
     component UART_TX_CTRL is
-        Port ( SEND : in  STD_LOGIC;
-               DATA : in  STD_LOGIC_VECTOR (7 downto 0);
-               CLK : in  STD_LOGIC;
-               READY : out  STD_LOGIC;
-               UART_TX : out  STD_LOGIC);
+        Port( 
+            SEND            : in  STD_LOGIC;
+            DATA            : in  STD_LOGIC_VECTOR (7 downto 0);
+            CLK             : in  STD_LOGIC;
+            READY           : out  STD_LOGIC;
+            UART_TX         : out  STD_LOGIC
+        );
     end component;
---State Type Decleration 
+    
+--STATE TYPE DECLERATIONS
     type CHAR_ARRAY is array (integer range<>) of std_logic_vector(7 downto 0); --a type defined as an array of std_logic_vector, this is what we use to send bytes over UART
     type UART_STATE_TYPE is (RST_REG, LD_INIT_STR, SEND_CHAR, RDY_LOW, WAIT_RDY, WAIT_BTN, LD_BTN_STR);
---SIGNALS and constants  
-constant TMR_CNTR_MAX : std_logic_vector(26 downto 0) := "101111101011110000100000000"; --100,000,000 = clk cycles per second
+--CONSTANTS 
+    constant TMR_CNTR_MAX : std_logic_vector(26 downto 0) := "101111101011110000100000000"; --100,000,000 = clk cycles per second
     constant TMR_VAL_MAX : std_logic_vector(3 downto 0) := "1001"; --9
     
     constant RESET_CNTR_MAX : std_logic_vector(17 downto 0) := "110000110101000000";-- 100,000,000 * 0.002 = 200,000 = clk cycles per 2 ms
@@ -181,7 +154,8 @@ constant TMR_CNTR_MAX : std_logic_vector(26 downto 0) := "1011111010111100001000
     constant ZERO : CHAR_ARRAY(0 to 2)       := (X"20", --'space'
                                                  X"30", --'1'
                                                  x"20");--'space'    
-                                                                                                
+                                                 
+--SIGNALS                                                                                                
     signal clock            : std_logic;
     signal reset            : std_logic                         := sw(3);--ck_rst;
     --btn(0) debounced
@@ -197,7 +171,8 @@ constant TMR_CNTR_MAX : std_logic_vector(26 downto 0) := "1011111010111100001000
     signal mem_block_out    : std_logic_vector(15 downto 0);
     signal reg_in           : std_logic_vector(15 downto 0)     := "1110000100000000";  
     signal random_out       : std_logic_vector(15 downto 0);
-    signal sys_clks_sec     : std_logic_vector(31 downto 0)     := "00000101111101011110000100000000";            
+    signal sys_clks_sec     : std_logic_vector(31 downto 0)     := "00000101111101011110000100000000";      
+          
 --UART_TX_CTRL control signals
     signal uartRdy : std_logic;
     signal uartSend : std_logic := '0';
@@ -222,7 +197,8 @@ constant TMR_CNTR_MAX : std_logic_vector(26 downto 0) := "1011111010111100001000
 --These signals are used to time the UART output so that it is readable
     constant tmr_val  :   integer := 50000000;
     signal tmr         :  integer :=0;
-    signal tmr_tgl      : std_logic;                                                              
+    signal tmr_tgl      : std_logic;      
+                                                            
 begin    
    
     PUSH_BUTTON_ZERO    :   btn_debounce
@@ -264,25 +240,13 @@ begin
             sel         => sw(1 downto 0), 
             clk_out     => clock
         );
-    
-    LED_DECODE      :   led_decoder
+        
+    SEVEN_SEGMENT_DISPLAY      :   char_decoder
         port map(
             clk         => clock,
             reset       => reset,
-            mem_block   => mem_block_out(7 downto 0), --random_out(7 downto 0),
-            led         => led,               
-            led0_b      => led0_b,
-            led0_g      => led0_g,
-            led0_r      => led0_r,
-            led1_b      => led1_b,
-            led1_g      => led1_g,
-            led1_r      => led1_r,
-            led2_b      => led2_b,
-            led2_g      => led2_g,
-            led2_r      => led2_r,
-            led3_b      => led3_b,
-            led3_g      => led3_g,      
-            led3_r      => led3_r
+            bin_in      => mem_block_out,
+            hex_out     => seg
         );
         
     SHIFT_REG       :   lfsr
@@ -303,28 +267,11 @@ begin
             data_in     => random_out,
             data_out    => mem_block_out
         );
- UART_TMR   :   process(CLK100MHZ)
-    begin
-            if(rising_edge(CLK100MHZ)) then
-                if(tmr = tmr_val) then --top most check, if our timer has reached it's val then we reset the time, toggle the toggle
-                    tmr <= 0;
-                    tmr_tgl <= '1';
-                elsif(tmr < tmr_val and tmr_tgl = '1') then --this ensures the toggle bit is only set for one CLK100MHZ cycle
-                    tmr_tgl <= '0';
-                else
-                    tmr <= tmr + 1;     --if our timer hasn't counted up and our toggle bit is 0 then we increment the timer value
-                end if;
-            end if;
-    end process;
- process(CLK100MHZ)
-        begin
-            if rising_edge(CLK100MHZ) then
-            dataCHAR(0) <= mem_block_in(15 downto 8);
-            dataCHAR(1) <= mem_block_in(7 downto 0);
-            else
-            end if;
-        end process;       
-----------------------------------------------------------
+
+        an <= "0111";
+        dp <= '1';
+               
+        ----------------------------------------------------------
         ------              UART Control                   -------
         ----------------------------------------------------------
         --Messages are sent on reset and when a button is pressed.
@@ -333,6 +280,31 @@ begin
         --will complete transmission of any byte that may have been initiated during 
         --FPGA configuration due to the UART_TX line being pulled low, preventing a 
         --frame shift error from occuring during the first message.
+        
+        UART_TMR   :   process(CLK100MHZ)
+            begin
+                if(rising_edge(CLK100MHZ)) then
+                    if(tmr = tmr_val) then --top most check, if our timer has reached it's val then we reset the time, toggle the toggle
+                        tmr <= 0;
+                        tmr_tgl <= '1';
+                    elsif(tmr < tmr_val and tmr_tgl = '1') then --this ensures the toggle bit is only set for one CLK100MHZ cycle
+                        tmr_tgl <= '0';
+                    else
+                        tmr <= tmr + 1;     --if our timer hasn't counted up and our toggle bit is 0 then we increment the timer value
+                    end if;
+                end if;
+        end process;
+        
+        process(CLK100MHZ)
+            begin
+                if rising_edge(CLK100MHZ) then
+                    dataCHAR(0) <= mem_block_in(15 downto 8);
+                    dataCHAR(1) <= mem_block_in(7 downto 0);
+                else
+                end if;
+        end process;
+        
+        
         process(CLK100MHZ)
         begin
           if (rising_edge(CLK100MHZ)) then
