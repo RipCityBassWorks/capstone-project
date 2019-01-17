c4434b5b4cf7369360f6a065bc80fa1acfb35fb9
Stefan Andersson <ripcitybassworks@gmail.com>
2019-01-16 19:56:00 -0700

    Changed 1kHz option to 100Hz

--- clock_divider.vhd
+++ clock_divider.vhd
@@ -7,7 +7,13 @@
 -- Project Name: capstone-fpga-memory-model
 -- Target Devices: XC7A35TICSG324-1L
 -- Tool Versions: Vivado 2018.2
--- Description: 4 option clock divider using counters
+-- Description: 
+-- 4 option clock divider using counters
+-- Controled by SW(1 downto 0):
+-- 00 = 2Hz
+-- 01 = 5Hz
+-- 10 = 10Hz
+-- 11 = 100Hz
 -- Component of xc7_top_level.vhd
 ----------------------------------------------------------------------------------
 
@@ -29,59 +35,59 @@ end entity clock_divider;
 architecture clock_divider_arch of clock_divider is
 
 --SIGNALS    
-    signal cnt_int      : integer;
+    signal count        : integer;
     signal clk          : std_logic;
     
 begin
 
     process(clk_in, reset)
         begin
-            if(reset = '0') then
-                cnt_int <= 0;
+            if(reset = '1') then
+                count <= 0;
                 clk     <= '0';
             elsif(rising_edge(clk_in)) then
                 --2Hz clock
                 --period = 0.5sec
                 --counts = 49,999,999
                 if(sel = "00") then
-                    if(cnt_int = 49999999) then
-                        cnt_int <= 0;
+                    if(count = 49999999) then
+                        count <= 0;
                         clk     <= not clk;
                     else
-                        cnt_int <= cnt_int + 1;
+                        count <= count + 1;
                     end if;
                     
                 --5Hz clock
                 --period = 0.2sec
                 --counts = 19,999,999
                 elsif(sel = "01") then
-                    if(cnt_int = 19999999) then
-                        cnt_int <= 0;
+                    if(count = 19999999) then
+                        count <= 0;
                         clk     <= not clk;
                     else
-                        cnt_int <= cnt_int + 1;
+                        count <= count + 1;
                     end if;
                     
                 --10Hz clock
                 --period = 0.1sec
                 --counts = 10,000,000
                 elsif(sel = "10") then
-                    if(cnt_int = 9999999) then
-                        cnt_int <= 0;
+                    if(count = 9999999) then
+                        count <= 0;
                         clk     <= not clk;
                     else
-                        cnt_int <= cnt_int + 1;
+                        count <= count + 1;
                     end if;
                     
-                --10kHz clock
-                --period = 0.0001sec
-                --counts = 10,000
+                --100Hz clock
+                --period = 0.01sec
+                --counts = 100,000
                 elsif(sel = "11") then
-                    if(cnt_int = 9999) then
-                        cnt_int <= 0;
+                    if(count = 999999) then
+                        count <= 0;
                         clk     <= not clk;
                     else
-                        cnt_int <= cnt_int + 1;
+                        count <= count + 1;
                     end if;
                 end if;
             end if;
