8331d6dc06fde0a931d94a490091d3e5d1ae6672
Stefan Andersson <ripcitybassworks@gmail.com>
2019-04-04 19:06:16 -0600

    Removed delay

--- lfsr.vhd
+++ lfsr.vhd
@@ -12,8 +12,8 @@
 -- Linear Feedback Shift Register 
 -- for pseudo random number generation
 --
--- User input can insert a 1 in a slot of the vector
--- Determined by a 16 digit counter 
+-- An enable condition flips a bit determined 
+-- by the current value of a 16 digit counter 
 -- Component of xc7_top_level.vhd
 ----------------------------------------------------------------------------------
 
@@ -25,31 +25,27 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity lfsr is
     port(
         clk         : in    std_logic;
-        clock       : in    std_logic;
         reset       : in    std_logic;
         en          : in    std_logic;
-        delay       : in    std_logic;
         reg_in      : in    std_logic_vector(15 downto 0);
         en_out      : out   std_logic;
-        lfsr_out    : out   std_logic_vector(15 downto 0);
-        random_out  : out   std_logic_vector(15 downto 0)
+        lfsr_out    : out   std_logic_vector(15 downto 0)
     );
 end entity lfsr;
 
 architecture lfsr_arch of lfsr is
 
---COMPONENT DECLARATIONS  
-
-    
 --SIGNALS    
     signal shift    	: std_logic_vector(15 downto 0)  := reg_in;
-    signal xor_out      : std_logic;
     signal count        : integer;
-    signal random_flag  : std_logic;
+    
 
 begin
+     
        
-       
+    --16 bit Fibonacci configuration LFSR
+    --bit(15) = (((bit(0) XOR bit(2)) xor bit(3)) XOR bit(5))  
+    --Right shift toward LSB 
     LINEAR_FEEDBACK_SHIFT_REGISTER  :   process(clk, reset, en, reg_in)
         begin
             if(reset = '1') then
@@ -57,26 +53,24 @@ begin
             elsif(rising_edge(clk)) then
                 if(en = '1') then
                     shift(count) <= not shift(count);
-                    random_out <= shift;
-                    random_flag <= '1';
-                end if;
-                if(delay = '1') then
-                    en_out <= random_flag;
-                    xor_out <= shift(10) xor (shift(12) xor (shift(15) xor shift(13)));
+                    en_out <= '1';
+                    shift(15) <= (((shift(0) xor shift(2)) xor shift(3)) xor shift(5));
                     shift(14 downto 0) <= shift(15 downto 1);
-                    shift(15) <= xor_out;
                     lfsr_out <= shift;
-                    random_flag <= '0';
+                else
+                    shift(15) <= (((shift(0) xor shift(2)) xor shift(3)) xor shift(5));
+                    shift(14 downto 0) <= shift(15 downto 1);
+                    lfsr_out <= shift;
+                    en_out <= '0';
                 end if;
             end if;
     end process;
     
     
-    EVENT_COUNTER   :   process(clock, reset) 
+    --16 digit counter for modifying the LFSR based on external input
+    EVENT_COUNTER   :   process(clk, reset) 
         begin
-            if(reset = '1') then
-                count <= 0;
-            elsif(rising_edge(clock)) then
+            if(rising_edge(clk)) then
                 if(count = 15) then
                     count <= 0;
                 else
